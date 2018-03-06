caafd5530c759d4288cb0b3a945309b8755a4c14
ok <oleg.koleskin@gmail.com>
2018-03-06 21:06:36 +0200

    WIP

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ const telegramBot = require('./services/telegramBot');
 
   const print = async (msg) => {
     if ('outboundAccountInfo' === msg.e) {
-      let arr = msg.B.filter(el => (el.f > 0 || el.l > 0));
+      let arr = msg.B.filter(el => (el.f > 0 || el.l > 0)).map(el => JSON.stringify(el));
       await telegramBot.sendMessage('Update account info is received - ' + JSON.stringify(arr));
       console.log(`RECEIVED: ${JSON.stringify(arr)}`);
     } else if ('executionReport' === msg.e) {
